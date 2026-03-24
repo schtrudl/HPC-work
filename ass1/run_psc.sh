e257bb72f7ec99bf7a65e4c32271c125ef3f481f
schtrudl <125376055+schtrudl@users.noreply.github.com>
2026-03-24 08:02:36 +0100

    result simd16
    Signed-off-by: schtrudl <125376055+schtrudl@users.noreply.github.com>

--- ass1/run_psc.sh
+++ ass1/run_psc.sh
@@ -3,8 +3,8 @@
 #SBATCH --reservation=fri
 #SBATCH --job-name=psc
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
-#SBATCH --output=timings.native2.log
+#SBATCH --cpus-per-task=16
+#SBATCH --output=timings.simd16.log
 #SBATCH --hint=nomultithread
 
 # Set OpenMP environment variables for thread placement and binding
