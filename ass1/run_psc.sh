f1b6854560f14d59bfd4d6291fe34d610bbd18a0
schtrudl <125376055+schtrudl@users.noreply.github.com>
2026-03-24 16:19:08 +0100

    last snap
    Signed-off-by: schtrudl <125376055+schtrudl@users.noreply.github.com>

--- ass1/run_psc.sh
+++ ass1/run_psc.sh
@@ -3,11 +3,10 @@
 #SBATCH --reservation=fri
 #SBATCH --job-name=psc
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --output=timings.final.log
+#SBATCH --cpus-per-task=1
+#SBATCH --output=timings.final1.log
 #SBATCH --hint=nomultithread
 #SBATCH --time=01:30:00
-#SBATCH --mem=128G
 
 # Set OpenMP environment variables for thread placement and binding
 export OMP_PLACES=cores
@@ -22,6 +21,4 @@ rm sample
 # Compile
 gcc -O3 -march=native -lm -lnuma --openmp sample.c -o sample
 
-for i in 2 4 8 16 32; do
-    OMP_NUM_THREADS=$i srun python3 run.py -n 20 >> timings.final$i.log
-done
+srun python3 run.py -n 20
