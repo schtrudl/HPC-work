f7b9993e0e64190aa70975be1f5ef1a2db11689c
schtrudl <125376055+schtrudl@users.noreply.github.com>
2026-04-11 10:23:00 +0200

    fast math
    Signed-off-by: schtrudl <125376055+schtrudl@users.noreply.github.com>

--- ass2/run_lenia_cpu.sh
+++ ass2/run_lenia_cpu.sh
@@ -4,20 +4,23 @@
 #SBATCH --job-name=lenia_cpu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH --output=timings_cpu_8.log
+#SBATCH --output=timings_cpu_8_fast_math.log
 #SBATCH --hint=nomultithread
-#SBATCH --time=10:00:00
+#SBATCH --time=01:00:00
 
 # Set OpenMP environment variables for thread placement and binding
 export OMP_PLACES=cores
 export OMP_PROC_BIND=close
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-for i in 2 4 8 16 32 64; do
-    echo "Run $i"
-    export OMP_NUM_THREADS=$i
-    ./run.py cpu -n=7 --srun > timings_cpu_c${i}.log
-done
+#for i in 2 4 8 16 32 64; do
+#    echo "Run $i"
+#    export OMP_NUM_THREADS=$i
+#    ./run.py cpu -n=7 --srun > timings_cpu_c${i}.log
+#done
 
-#./run.py cpu -n=10 --srun
+./run.py cpu -n=20 --srun
+
+#module load FFmpeg
+#./verify.py --srun cpu
 
