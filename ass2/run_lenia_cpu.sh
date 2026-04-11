509bfb51246d251f2cc9fc0b726214046fe2f2ab
schtrudl <125376055+schtrudl@users.noreply.github.com>
2026-04-11 09:39:41 +0200

    dump
    Signed-off-by: schtrudl <125376055+schtrudl@users.noreply.github.com>

--- ass2/run_lenia_cpu.sh
+++ ass2/run_lenia_cpu.sh
@@ -3,15 +3,21 @@
 #SBATCH --reservation=fri
 #SBATCH --job-name=lenia_cpu
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --output=timings_cpu_7_LUT.log
+#SBATCH --cpus-per-task=64
+#SBATCH --output=timings_cpu_8.log
 #SBATCH --hint=nomultithread
+#SBATCH --time=10:00:00
 
 # Set OpenMP environment variables for thread placement and binding
 export OMP_PLACES=cores
 export OMP_PROC_BIND=close
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
-#RUN
-./run.py cpu -n=10 --srun
+for i in 2 4 8 16 32 64; do
+    echo "Run $i"
+    export OMP_NUM_THREADS=$i
+    ./run.py cpu -n=7 --srun > timings_cpu_c${i}.log
+done
+
+#./run.py cpu -n=10 --srun
 
