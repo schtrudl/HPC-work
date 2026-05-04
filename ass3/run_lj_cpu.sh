b77009390e24750ee86b0e9cb256d366fd3d5e7e
schtrudl <125376055+schtrudl@users.noreply.github.com>
2026-05-04 18:59:57 +0200

    tole mi dej
    Signed-off-by: schtrudl <125376055+schtrudl@users.noreply.github.com>

--- ass3/run_lj_cpu.sh
+++ ass3/run_lj_cpu.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --hint=nomultithread
-#SBATCH --output=timings_cpu_8_better_math.log
+#SBATCH --output=timings_cpu_9_tiling.log
 #SBATCH --time=01:00:00
 
 # Set OpenMP environment variables for thread placement and binding
@@ -19,10 +19,10 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 #    ./run.py cpu -n=5 --srun > timings_cpu_c${i}.log
 #done
 
-rm -f lj_cpu lj_cpu2
+rm -f lj_cpu lj_cpu
 module load CUDA
-./run.py --srun cpu2 -n=10
+./run.py --srun cpu -n=10
 
 #module load FFmpeg
-#./verify.py --srun cpu2 --full
+#./verify.py --srun cpu --full
 
