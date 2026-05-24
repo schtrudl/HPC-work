fbf0c43d9772088fc024f0f834954f863ec3529b
schtrudl <125376055+schtrudl@users.noreply.github.com>
2026-05-24 06:10:37 +0200

    mpc
    Signed-off-by: schtrudl <125376055+schtrudl@users.noreply.github.com>

---
+++ ass4/run_lenia2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --reservation=fri
+#SBATCH --job-name=lenia
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=1
+#SBATCH --output=timings_x.log
+#SBATCH --hint=nomultithread
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=00:30:00
+
+#Load MPI module
+#module load FFmpeg
+module load OpenMPI
+#mpirun -np $SLURM_NTASKS ./lenia
+#./run.py -n 5 --binary lenia-mpc
+./verify.py --binary lenia-mpc
+
+#mpicc -O3 -Wall sm.c -o sm -lm
+#mpirun --mca pml ob1 -np $SLURM_NTASKS ./sm
